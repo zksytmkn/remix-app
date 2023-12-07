85bdcba6253b16b329c05fad1209c3fbf1744888
nakamoto <zksytmkn4869@gmail.com>
2023-12-08 01:59:30 +0900

    🐛

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node", "prettier", "plugin:tailwindcss/recommended"],
   plugins: ["tailwindcss"],
   settings: {
-    tailwindcss: { callees: ["cn"], config: "tailwind.config.mjs" },
+    tailwindcss: { callees: ["cn"], config: "tailwind.config.ts" },
   },
   overrides: [{ files: ["*.ts", "*.tsx"], parser: "@typescript-eslint/parser" }],
 };
